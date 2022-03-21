1769b7ef961977371496b7a39ada90a322b34c3c
marvingonzaga <francismiguel053118@gmail.com>
2022-03-22 03:13:59 +0800

    update commit

--- TrialApp/Form1.Designer.cs
+++ TrialApp/Form1.Designer.cs
@@ -54,7 +54,7 @@
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(69, 20);
             this.label1.TabIndex = 1;
-            this.label1.Text = "Version 2";
+            this.label1.Text = "Version 3";
             // 
             // pictureBox1
             // 
